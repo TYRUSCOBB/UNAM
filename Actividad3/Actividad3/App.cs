138a45d176a9979bb07c8be99b6796e49f8a62b6
Eduardo Castillo Madrigal <edu_castillo@hotmail.com>
2015-09-10 02:32:09 -0500

    Cambie Hello Forms a español y cambiel el color del texto a verde y el fondo a amarillo

--- Actividad3/Actividad3/App.cs
+++ Actividad3/Actividad3/App.cs
@@ -5,15 +5,19 @@ namespace Actividad3
 {
 	public class App
 	{
+		
 		public static Page GetMainPage ()
-		{	
+		{ 
 			return new ContentPage { 
 				Content = new Label {
-					Text = "Hello, Forms!",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					Text = "Hola, Formas !  ",
+					VerticalOptions = LayoutOptions.End  ,
+					HorizontalOptions = LayoutOptions.StartAndExpand   ,
+					TextColor = Color.Green,
+					BackgroundColor = Color.Yellow 
 				},
 			};
+
 		}
 	}
 }
